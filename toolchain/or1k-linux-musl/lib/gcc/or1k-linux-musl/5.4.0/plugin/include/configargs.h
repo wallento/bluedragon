dc8524c0b4f70114ead4ce8e0585234ac12bd9da
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2017-05-11 19:06:32 -0300

    adding toolchain

---
+++ toolchain/or1k-linux-musl/lib/gcc/or1k-linux-musl/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/crossbuild/output/or1k-linux-musl --target=or1k-linux-musl --enable-languages=c,c++ --disable-libmudflap --disable-libsanitizer --disable-nls --disable-multilib --with-multilib-list=";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
---
+++ toolchain/or1k-linux/lib/gcc/or1k-linux/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/opt/crossbuild/linux-nolib/or1k-gcc-or1k-5.4.0/configure --target=or1k-linux --enable-targets=all --prefix=/opt/crossbuild/output/or1k-linux --enable-languages=c --without-headers --disable-bootstrap --enable-sjlj-exceptions --with-system-libunwind --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
