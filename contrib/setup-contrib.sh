b6a892762643c0bb4e5db520e6fa44c9ad1eb103
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2017-05-12 11:09:46 -0300

    addinc contrib

---
+++ contrib/setup-contrib.sh
@@ -0,0 +1,74 @@
+# Number of available cores.
+ncores=`grep -c ^processor /proc/cpuinfo`
+
+# Base directory.
+BASEDIR=$1
+
+# Contrib tools.
+QEMU=qemu-2.9.0
+
+#==============================================================================
+# usage()
+#==============================================================================
+
+#
+# Prints script usage and exits.
+#
+function usage
+{
+	echo "setup-contrib.sh <basedir>"
+	exit
+}
+
+#==============================================================================
+# setup_qemu()
+#==============================================================================
+
+#
+# Setups QEMU.
+#   $1 Installation directory.
+#
+function setup_qemu
+{
+	local basedir=`pwd`
+	local installdir=$basedir/$1
+
+	# Create installation directory.
+	mkdir -p $installdir
+
+	echo $installdir
+
+	cd contrib
+
+	tar -xjvf $QEMU.tar.bz2
+
+	cd $QEMU
+
+	mkdir build
+	cd build
+
+	../configure                   \
+		--prefix=$installdir       \
+		--target-list=or1k-softmmu \
+
+	make -j $ncores all
+
+	make install
+
+	cd ../..
+	rm -rf $QEMU
+	
+	cd $basedir
+}
+
+#==============================================================================
+# MAIN
+#==============================================================================
+
+# Mussing base directory.
+if [ -z $BASEDIR ]; then
+	echo "missing base directory"
+	usage
+fi
+
+setup_qemu $BASEDIR/qemu
