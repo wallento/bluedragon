05e69e7102ff68267e2275ff451882badad73e48
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2017-06-01 17:39:10 -0300

    adding device buffers

--- contrib/qemu-2.9.0/hw/char/optimsoc-na.c
+++ contrib/qemu-2.9.0/hw/char/optimsoc-na.c
@@ -34,10 +34,12 @@
 #include "qemu/error-report.h"
 #include "hw/char/optimsoc-na.h"
 
+#define BUFSIZE 4096
+
 /*
  * Number of endpoints.
  */
-#define OPTIMSOC_NUMEP 256
+#define OPTIMSOC_NA_NUMEP 16
 
 /*
  * OpTiMSoC NoC Adapter base address.
@@ -76,6 +78,19 @@
 #define OPTIMSOC_NA_REG_ENABLE(i) \
 	(OPTIMSOC_NA_EP_BASE + (i)*OPTIMSOC_NA_EP_REGSIZE + 4)
 
+#define DEBUG(str) \
+	fprintf(stdout, "[QEMU] %s\n", (str));
+
+/**
+ * Device buffers.
+ */
+struct buffer
+{
+	int head;               /* Index to head data. */
+	int tail;               /* Index to tail data. */
+	uint32_t data[BUFSIZE]; /* Data.               */
+};
+
 /*
  * NoC Adapter information.
  */
@@ -90,26 +105,115 @@ static struct
 	hwaddr base;             /* Base address.            */
 	qemu_irq irq;            /* IRQ.                     */
 } info = {
-	OPTIMSOC_NUMEP,
+	OPTIMSOC_NA_NUMEP,
 };
 
+struct buffer in_buffers[OPTIMSOC_NA_NUMEP];
+struct buffer out_buffers[OPTIMSOC_NA_NUMEP];
+
+/*============================================================================*
+ * Buffer Operations                                                          *
+ *============================================================================*/
+
 /*
- * Checks for the memory operation bounds.
+ * Initializes a buffer.
+ *
+ * @para buf Target buffer.
  */
+static void buffer_init(struct buffer *buf)
+{
+	buf->head = 0;
+	buf->tail = 0;
+}
+
+/*
+ * Asserts if a buffer is empty.
+ *
+ * @param buf target buffer.
+ */
+static int buffer_empty(struct buffer *buf)
+{
+	return (buf->head == buf->tail);
+}
+
+/*
+ * Asserts if a buffer is full.
+ *
+ * @param buf target buffer.
+ */
+static int buffer_full(struct buffer *buf)
+{
+	return ((buf->tail + 1)%BUFSIZE == buf->head);
+}
+
+/*
+ * Reads a word from the target buffer.
+ *
+ * @addr target address.
+ */
+static uint32_t buffer_read(hwaddr addr)
+{
+	int i;
+	int ep;
+	uint32_t word;
+	
+	ep = (addr - info.numep)/OPTIMSOC_NA_REGSIZE;
+
+	/* Empty buffer. */
+	if (buffer_empty(&in_buffers[ep]))
+		return (0);
+
+	i = in_buffers[ep].head;
+	word = in_buffers[ep].data[i];
+	in_buffers[ep].head = (in_buffers[ep].head + 1)%BUFSIZE;
+
+	return (word);
+}
+
+/*
+ * Writes a word to the target buffer.
+ *
+ * @addr target address.
+ */
+static void buffer_write(hwaddr addr, uint32_t word)
+{
+	int i;
+	int ep;
+	
+	ep = (addr - info.numep)/OPTIMSOC_NA_REGSIZE;
+
+	/* Empty buffer. */
+	if (buffer_full(&out_buffers[ep]))
+	{
+		DEBUG("out buffer overflow");
+		return;
+	}
+
+	i = in_buffers[ep].tail;
+	in_buffers[ep].data[i] = word;
+	in_buffers[ep].tail = (in_buffers[ep].tail + 1)%BUFSIZE;
+}
+
+/*============================================================================*
+ * Memory Mapped IO Operations                                                *
+ *============================================================================*/
 
 /*
  * Memory read callback.
  */
 static uint64_t optimsoc_na_mm_read(void *opaque, hwaddr addr, unsigned size)
 {
-	uint64_t ret = 0;
+	/* Sanity check. */
+	if (size != 4)
+		DEBUG("bad size for read request");
 
+	/* Read number of entry points register. */
 	if (addr == OPTIMSOC_NA_REG_NUMEP)
-		ret = OPTIMSOC_NUMEP;
+		return (info.numep);
 
-	fprintf(stderr, "[QEMU] take my data %x %" PRIu64 "\n", addr, ret);
+	DEBUG("optimsoc read");
 
-	return (ret);
+	return (buffer_read(addr));
 }
 
 /*
@@ -117,7 +221,13 @@ static uint64_t optimsoc_na_mm_read(void *opaque, hwaddr addr, unsigned size)
  */
 static void optimsoc_na_mm_write(void *opaque, hwaddr addr, uint64_t value, unsigned size)
 {
-	fprintf(stderr, "[QEMU] feed me with data %" PRIu64 "\n", value);
+	/* Sanity check. */
+	if (size != 4)
+		DEBUG("bad size for write request");
+
+	DEBUG("optimsoc write");
+
+	buffer_write(addr, value);
 }
 
 /*
@@ -142,9 +252,16 @@ void optimsoc_na_mm_init(MemoryRegion *addrspace, hwaddr base, qemu_irq irq)
 	info.base = base;
 	info.irq = irq;
 
+	/* Initialize device buffers. */
+	for (int i = 0; i < OPTIMSOC_NA_NUMEP; i++)
+	{
+		buffer_init(&in_buffers[i]);
+		buffer_init(&out_buffers[i]);
+	}
+
 	/* Initializes memory mapped IO region. */
     memory_region_init_io(&info.io, NULL, &optimsoc_na_mm_ops, NULL, "optimsoc-na",
-		OPTIMSOC_NUMEP*(OPTIMSOC_NA_REGSIZE + 1));
+		OPTIMSOC_NA_NUMEP*(OPTIMSOC_NA_REGSIZE + 1));
 
 	/* Attach memory mapped IO region to the virtual address space. */
     memory_region_add_subregion(info.addrspace, info.base, &info.io);
