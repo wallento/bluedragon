290c0e034494dd5e221e4adae473f647140e9084
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2017-07-12 10:15:59 -0300

    custom init

---
+++ initramfs/main.c
@@ -0,0 +1,107 @@
+/**
+ * Copyright(C) 2017 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * @brief NoC major device number.
+ */
+#define NOC_MAJOR 253
+
+/**
+ * @brief NoC minor device number.
+ */
+#define NOC_MINOR 0
+
+/**
+ * Buffer size.
+ */
+#define BUF_SIZE 128
+
+/**
+ * @brief NoC device filename.
+ */
+const char *devname = "/dev/noc";
+
+/**
+ * @brief Panics the utility.
+ */
+static void panic(void)
+{
+	perror(NULL);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * @brief Initializes NoC device.
+ */
+static void init_noc(const char *pathname)
+{
+	int ret;   /* Return values of syscalls. */
+	dev_t dev; /* NoC device number,         */
+
+	printf("%-30s", "initializing noc device...");
+
+	/* Create device file. */
+	dev = makedev(NOC_MAJOR, NOC_MINOR);
+	ret = mknod(pathname, S_IFCHR | S_IRUSR | S_IWUSR, dev);
+	if (ret != 0)
+		panic();
+
+	printf("[done]\n");
+}
+
+int main(int argc, char **argv)
+{
+	int fd;             /* NoC device file descriptor. */
+	int ret;            /* Return value of syscalls    */
+	char buf[BUF_SIZE]; /* Buffer for I/O operations.  */
+
+	printf("Hello World!\n");
+
+	init_noc(devname);
+
+	/* Open NoC device. */
+	printf("%-30s", "opening noc device...");
+	fd = ret = open(devname, O_RDONLY);
+	if (ret < 0)
+		panic();
+	printf("[done]\n");
+
+	/* Read some data. */
+	printf("%-30s", "reading data...");
+	ret = read(fd, buf, 1);
+	if (ret < 0)
+		panic();
+	printf("[done]\n");
+
+	/* Close NoC device. */
+	printf("%-30s", "closing noc device...");
+	close(fd);
+	printf("[done]\n");
+
+	/* Stop here. */
+	while (1)
+		/* noop. */;
+
+	return (EXIT_SUCCESS);
+}
--- makefile
+++ makefile
@@ -4,7 +4,14 @@ export ARCH=openrisc
 
 export CROSS_COMPILE=or1k-linux-
 
-all: defconfig
+export CC=$(CURDIR)/tools/toolchain/or1k-linux-musl/bin/or1k-linux-musl-gcc
+
+
+export OUTDIR=$(CURDIR)/linux/arch/openrisc/initramfs
+
+.PHONY: initramfs
+
+all: defconfig initramfs
 	cd linux && \
 	$(MAKE)
 
@@ -16,6 +23,12 @@ modules: defconfig
 	cd linux &&       \
 	$(MAKE) modules
 
+initramfs:
+	mkdir -p $(OUTDIR)
+	$(CC) initramfs/*.c -static -o $(OUTDIR)/init
+	
+
 clean:
+	rm -rf $(OUTDIR)/init
 	cd linux &&       \
 	$(MAKE) clean
